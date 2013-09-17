4afd6e66b58cafdfa119f6d03670fbc6148be020
vasech <vassili.echser@centralway.com>
2013-09-17 11:44:18 +0200

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ttt::Application.config.secret_key_base = '2a24fd0ebb2965b6f36d02e9b81f2ed6bd470d02000e784423b1ac32c8ca359b4456ab317c7a95dc143114a8f7d52daf5d0f22b7ff9ac7dd6185f8dd5c7784bd'
